03f6b9edf2a6f483b2cc2573db44174cd8a94880
Nikolay Trifonov <koksibg@students.softuni.bg>
2017-05-25 00:01:53 +0300

    Add project files.

---
+++ Add_Two_Numbers/Add_Two_Numbers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Add_Two_Numbers
+{
+    class Add_Two_Numbers
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{numberOne} + {numberTwo} = {numberOne + numberTwo}");
+        }
+    }
+}
---
+++ Employee_Data/Employee_Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Employee_Data
+{
+    class Employee_Data
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            byte age = byte.Parse(Console.ReadLine());
+            int idNumber = int.Parse(Console.ReadLine());
+            double monthlySalary = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Age: {age}");
+            Console.WriteLine($"Employee ID: {idNumber:D8}");
+            Console.WriteLine($"Salary: {monthlySalary:F2}");
+        }
+    }
+}
